643fb186400d145a4f4d7e5e47adcf5d26b24f13
ncluff003 <ncluff003@zohomail.com>
2021-08-02 11:29:21 -0600

    Added functionality to the Login & Signup buttons to toggle their respective forms.

--- Public/DIST/Bundle.js
+++ Public/DIST/Bundle.js
@@ -12,6 +12,23 @@ questions.forEach(function (question, i) {
     questionLabels[i].style.display = 'none';
   }
 });
+var questionContainer = document.querySelector('.signup-form_questions-and-submit-container--question');
+var buttonContinue = document.querySelector('.signup-form_questions-and-submit-container--continue');
+buttonContinue.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(questions[0].value);
+});
+var closeSignupButton = document.querySelector(".signup-form-container--close");
+var signupButton = document.querySelector(".navigation_user-login_signup");
+var loginButton = document.querySelector(".navigation_user-login_login");
+var signupFormContainer = document.querySelector('.signup-form-container');
+var loginFormContainer = document.querySelector(".navigation_login-form-container");
+signupButton.addEventListener("click", function () {
+  signupFormContainer.classList.toggle("signup-form-container--shown");
+});
+loginButton.addEventListener("click", function () {
+  loginFormContainer.classList.toggle("navigation_login-form-container--shown");
+});
 /******/ })()
 ;
 //# sourceMappingURL=Bundle.js.map
--- Public/JS/App.js
+++ Public/JS/App.js
@@ -1,5 +1,6 @@
 const questions = document.querySelectorAll(`.question`);
 const questionLabels = document.querySelectorAll(`.question-label`);
+
 console.log(questions, questionLabels);
 questions.forEach((question, i) => {
   if (question === questions[0]) return;
@@ -7,4 +8,26 @@ questions.forEach((question, i) => {
     question.style.display = 'none';
     questionLabels[i].style.display = 'none';
   }
+});
+
+const questionContainer = document.querySelector('.signup-form_questions-and-submit-container--question');
+const buttonContinue = document.querySelector('.signup-form_questions-and-submit-container--continue');
+
+buttonContinue.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  console.log(questions[0].value);
+});
+
+const closeSignupButton = document.querySelector(`.signup-form-container--close`);
+const signupButton = document.querySelector(`.navigation_user-login_signup`);
+const loginButton = document.querySelector(`.navigation_user-login_login`);
+const signupFormContainer = document.querySelector('.signup-form-container');
+const loginFormContainer = document.querySelector(`.navigation_login-form-container`);
+
+signupButton.addEventListener(`click`, function () {
+  signupFormContainer.classList.toggle(`signup-form-container--shown`);
+});
+
+loginButton.addEventListener(`click`, function () {
+  loginFormContainer.classList.toggle(`navigation_login-form-container--shown`);
 });
