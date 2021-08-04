74c2ddd5a0bdc3a9772187bdcaf1d5d42ff8aa89
ncluff003 <ncluff003@zohomail.com>
2021-08-04 13:40:47 -0600

    Encapsulated Public JS into an App class for a bit more organization.

--- Public/DIST/Bundle.js
+++ Public/DIST/Bundle.js
@@ -3,41 +3,76 @@ var __webpack_exports__ = {};
 /*!**************************!*\
   !*** ./Public/JS/App.js ***!
   \**************************/
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 var questions = document.querySelectorAll(".question");
 var questionLabels = document.querySelectorAll(".question-label");
-console.log(questions, questionLabels);
-questions.forEach(function (question, i) {
-  if (question === questions[0]) return;else {
-    question.style.display = 'none';
-    questionLabels[i].style.display = 'none';
-  }
-});
 var questionContainer = document.querySelector('.signup-form_questions-and-submit-container--question');
 var buttonContinue = document.querySelector('.signup-form_questions-and-submit-container--continue');
-buttonContinue.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(questions[0].value);
-});
 var closeSignupButton = document.querySelector(".signup-form-container--close");
 var signupButton = document.querySelector(".navigation_user-login_signup");
 var loginButton = document.querySelector(".navigation_user-login_login");
 var signupFormContainer = document.querySelector('.signup-form-container');
 var loginFormContainer = document.querySelector(".navigation_login-form-container");
 
-var getFocus = function getFocus(element) {
-  element.focus();
-};
+var App = /*#__PURE__*/function () {
+  function App() {
+    _classCallCheck(this, App);
 
-signupButton.addEventListener("click", function () {
-  signupFormContainer.classList.toggle("signup-form-container--shown");
-  var firstName = document.querySelector(".first-name");
-  getFocus(firstName);
-});
-loginButton.addEventListener("click", function () {
-  loginFormContainer.classList.toggle("navigation_login-form-container--shown");
-  var userName = document.querySelector(".username");
-  getFocus(userName);
-});
+    console.log(questions);
+
+    this._watchButtons(loginButton, signupButton, buttonContinue);
+
+    this._setupNewUserQuestions(questions, questionLabels);
+  }
+
+  _createClass(App, [{
+    key: "_setupNewUserQuestions",
+    value: function _setupNewUserQuestions(questionArray, questionLabels) {
+      questions.forEach(function (question, i) {
+        if (question === questions[0]) return;else {
+          question.style.display = 'none';
+          questionLabels[i].style.display = 'none';
+        }
+      });
+    }
+  }, {
+    key: "_watchButtons",
+    value: function _watchButtons(loginButton, signupButton, continueButton) {
+      continueButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log(questions[0].value);
+      });
+      signupButton.addEventListener("click", function () {
+        signupFormContainer.classList.toggle("signup-form-container--shown");
+        var firstName = document.querySelector(".first-name");
+
+        app._getFocus(firstName);
+      });
+      loginButton.addEventListener("click", function () {
+        loginFormContainer.classList.toggle("navigation_login-form-container--shown");
+        var userName = document.querySelector(".username");
+
+        app._getFocus(userName);
+      });
+    }
+  }, {
+    key: "_getFocus",
+    value: function _getFocus(element) {
+      return element.focus(), this;
+    }
+  }]);
+
+  return App;
+}(); ///////////////////////////////////////////////
+// APP
+
+
+var app = new App();
 /******/ })()
 ;
 //# sourceMappingURL=Bundle.js.map
--- Public/JS/App.js
+++ Public/JS/App.js
@@ -1,41 +1,51 @@
 const questions = document.querySelectorAll(`.question`);
 const questionLabels = document.querySelectorAll(`.question-label`);
-
-console.log(questions, questionLabels);
-questions.forEach((question, i) => {
-  if (question === questions[0]) return;
-  else {
-    question.style.display = 'none';
-    questionLabels[i].style.display = 'none';
-  }
-});
-
 const questionContainer = document.querySelector('.signup-form_questions-and-submit-container--question');
 const buttonContinue = document.querySelector('.signup-form_questions-and-submit-container--continue');
-
-buttonContinue.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  console.log(questions[0].value);
-});
-
 const closeSignupButton = document.querySelector(`.signup-form-container--close`);
 const signupButton = document.querySelector(`.navigation_user-login_signup`);
 const loginButton = document.querySelector(`.navigation_user-login_login`);
 const signupFormContainer = document.querySelector('.signup-form-container');
 const loginFormContainer = document.querySelector(`.navigation_login-form-container`);
+class App {
+  constructor() {
+    console.log(questions);
+    this._watchButtons(loginButton, signupButton, buttonContinue);
+    this._setupNewUserQuestions(questions, questionLabels);
+  }
 
-const getFocus = (element) => {
-  element.focus();
-};
+  _setupNewUserQuestions(questionArray, questionLabels) {
+    questions.forEach((question, i) => {
+      if (question === questions[0]) return;
+      else {
+        question.style.display = 'none';
+        questionLabels[i].style.display = 'none';
+      }
+    });
+  }
 
-signupButton.addEventListener(`click`, function () {
-  signupFormContainer.classList.toggle(`signup-form-container--shown`);
-  const firstName = document.querySelector(`.first-name`);
-  getFocus(firstName);
-});
+  _watchButtons(loginButton, signupButton, continueButton) {
+    continueButton.addEventListener(`click`, function (e) {
+      e.preventDefault();
+      console.log(questions[0].value);
+    });
+    signupButton.addEventListener(`click`, function () {
+      signupFormContainer.classList.toggle(`signup-form-container--shown`);
+      const firstName = document.querySelector(`.first-name`);
+      app._getFocus(firstName);
+    });
+    loginButton.addEventListener(`click`, function () {
+      loginFormContainer.classList.toggle(`navigation_login-form-container--shown`);
+      const userName = document.querySelector(`.username`);
+      app._getFocus(userName);
+    });
+  }
 
-loginButton.addEventListener(`click`, function () {
-  loginFormContainer.classList.toggle(`navigation_login-form-container--shown`);
-  const userName = document.querySelector(`.username`);
-  getFocus(userName);
-});
+  _getFocus(element) {
+    return element.focus(), this;
+  }
+}
+
+///////////////////////////////////////////////
+// APP
+const app = new App();
